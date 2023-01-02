82e7d559ec0be4a1c3b2328e8b5a6e8897a70ef9
FredCoen <friedrich.kaspar.coen@gmail.com>
2023-01-02 20:01:58 +0100

    uniswa v3 spot price retrieval

---
+++ src/spot_price.rs
@@ -0,0 +1,23 @@
+use std::ops::Mul;
+
+use ethers::{
+    abi::{Int, Uint},
+    contract::Contract,
+    providers::Middleware,
+};
+
+pub async fn get_spot_price<M: Middleware>(
+    uniswap_pool: &Contract<M>,
+    token0_decimals: Uint,
+) -> Uint {
+    let (sqrt_ratio_x96, _, _, _, _, _, _) = uniswap_pool
+        .method::<_, (Uint, Int, Uint, Uint, Uint, Uint, bool)>("slot0", ())
+        .expect("Contract call failed")
+        .call()
+        .await
+        .expect("Retrieving sqrt_ratio_x96 failed");
+    return sqrt_ratio_x96
+        .pow(Uint::from(2))
+        .mul(Uint::from(10).pow(token0_decimals))
+        / Uint::from(2).pow(Uint::from(192));
+}
