2287ed30fceeebdc03dc830ef1a0597dd12a9aee
FredCoen <friedrich.kaspar.coen@gmail.com>
2023-01-02 23:07:40 +0100

    avoid overflow for large token decimals

--- src/spot_price.rs
+++ src/spot_price.rs
@@ -17,8 +17,6 @@ pub async fn get_spot_price<M: Middleware>(
         .await
         .expect("Retrieving sqrt_ratio_x96 failed");
 
-    return sqrt_ratio_x96
-        .pow(Uint::from(2))
-        .mul(Uint::from(10).pow(token0_decimals))
-        / Uint::from(2).pow(Uint::from(192));
+    return (sqrt_ratio_x96.pow(Uint::from(2)) / Uint::from(2).pow(Uint::from(192)))
+        .mul(Uint::from(10).pow(token0_decimals));
 }
